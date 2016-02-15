02c228972d95167f6e9791f7c866f98fd6a766ec
leaderman77 <admin@Mac-Admin-2.local>
2016-02-15 20:40:41 +0500

    Initial Commit

---
+++ Repositories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Repositories
+//
+//  Created by Admin on 2/15/16.
+//  Copyright (c) 2016 microapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Repositories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Repositories
+//
+//  Created by Admin on 2/15/16.
+//  Copyright (c) 2016 microapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
